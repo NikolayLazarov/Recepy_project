8524ff7e6e405b30daf765cebb12387fdb3728ec
Nikolay Lazarov <nikilazt7@gmail.com>
2022-05-29 17:07:01 +0300

    Avi views changes + Niki models

--- recipy_website/recepies/views.py
+++ recipy_website/recepies/views.py
@@ -4,125 +4,47 @@ from elasticsearch_dsl import Q
 from django.http import HttpResponse
 from recepies.documnets import RecepyDocument
 
+#pages 
 
+#the search
+def index(request): 
 
-def probe1(request):
-    recepts =  RecepyDocument.search().query()
-    return render(request, 'search.html',{'posts':recepts})
-
-
-def search_title(request):
-    query = request.GET.get('q')
-
-    if query:
-        q = Q(
-            'multi_match',
-            query = query,
-            fields = [ 
-                'title'
-            ]
-        )
-
-        posts = RecepyDocument.search().query(q)
-        response = posts.execute()
-    else:
-        posts = ''
-
-    return render(request, 'search.html',{'posts':posts})
-
-
-def all_data(request):
-    recepts =  RecepyDocument.search().query()
-    # cars =  CarDocument.search().query("match", title = "AUDI")
-    #                                        'for the html': from here
-    return render(request, 'all_data.html',{'posts':recepts})
-
-
-def search_title_text(request):
     query = request.GET.get('q')
 
     if query:
         q = Q(
             'bool',
-            # must = [
-            #     Q('match', name = query),
-            # ],
-            # must_not = [],
             should = [
                 Q('match',title = query),
                 Q('match', ingredients = query),
-                # Q('match', color = query),
-
             ],
             minimum_should_match = 1)
 
         posts = RecepyDocument.search().query(q)
-        # response = posts.execute()
     else:
         posts = ''
 
-    return render(request, 'search.html',{'posts':posts})
+    
+    print (posts)
+    return render(request, 'index.html', {'posts':posts})
 
-#pages Kris
+#all data
 def all_recepies(request):
-    recepts =  RecepyDocument.search().query()
-    # cars =  CarDocument.search().query("match", title = "AUDI")
-    #                                        'for the html': from here
+    recepts = RecepyDocument.search().query() 
     return render(request, 'recipes.html',{'posts':recepts})
 
+#result page
+def recipe_by_title (request, title):
+
+    recepts =  RecepyDocument.search().query("match", title=title)
+    return render(request, "single_recipe.html", {"recepts": recepts})
+
+#other
 def tags(request):
     return render(request, 'tags.html')
 
 def tag_template(request):
     return render(request, 'tag_template.html')
-
-
-def single_recipe(request):
-    query = request.GET.get('q')
-
-    if query:
-        q = Q(
-            'multi_match',
-            query = query,
-            fields = [
-                'title'
-            ]
-        )
-
-        posts = RecepyDocument.search().query(q)
-        response = posts.execute()
-    else:
-        posts = ''
-
-
-    return render(request, 'single_recipe.html',{'posts':posts})
-
-def index(request):
-
-    query = request.GET.get('q')
-
-    if query:
-        q = Q(
-            'bool',
-            # must = [
-            #     Q('match', name = query),
-            # ],
-            # must_not = [],
-            should = [
-                Q('match',title = query),
-                Q('match', ingredients = query),
-                # Q('match', color = query),
-
-            ],
-            minimum_should_match = 1)
-
-        posts = RecepyDocument.search().query(q)
-        # response = posts.execute()
-    else:
-        posts = ''
-
-    return render(request, 'index.html', {'posts':posts})
-
 
 def contact(request):
     recepts =  RecepyDocument.search().query()
@@ -134,25 +56,4 @@ def about(request):
 
 def error404(request):
     return render(request, '404.html')
-
-def recipe_by_title (request, title):
-    """print("it is okey")
-    print (title)
-    client = Elasticsearch()
-
-    s = Search(using=client)
-
-    s = Search().using(client).query("match", title=title)
-
-    response = s.execute()"""
-
-    recepts =  RecepyDocument.search().query("match", title=title)
-    
-    #print (response)
-    return render(request, "single_recipe.html", {"recepts": recepts})
-    
-
-    
-    #return render(request, 'single_recipe.html',{'post':s})
-
 
